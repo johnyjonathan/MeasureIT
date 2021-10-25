9439b637cb7cab19c2ce40d92990edcc828834b1
johnyjonathan <80420454+johnyjonathan@users.noreply.github.com>
2021-10-25 21:06:47 +0200

    v1.2 - settings

--- measureIT/settings.py
+++ measureIT/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'w8pi4o-6v-26u**ky&lmq2cwb*9j3x6=a^61)@74&rw&1(icad'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['measureit.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
